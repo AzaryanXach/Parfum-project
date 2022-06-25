7d535471b27dc22e75040fcfc0c1ac59dbfeb42b
Azaryan Khachik <azaryan.xachik.2@gmail.com>
2022-06-25 20:08:31 +0400

    New parfum project (Html, Css, Js) combined with each other

---
+++ js/monartParfums.js
@@ -0,0 +1,21 @@
+/*Left slider*/
+let offset = 0;
+const sliderLine = document.querySelector('.slider-line-left');
+
+document.querySelector('.slider-next-left').addEventListener('click', function(){
+    offset = offset + 256;
+    if (offset > 768) {
+        offset = 0;
+    }
+    sliderLine.style.left = -offset + 'px';
+});
+
+document.querySelector('.slider-prev-left').addEventListener('click', function () {
+    offset = offset - 256;
+    if (offset < 0) {
+        offset = 768;
+    }
+    sliderLine.style.left = -offset + 'px';
+});
+
+/*Right slider*/
